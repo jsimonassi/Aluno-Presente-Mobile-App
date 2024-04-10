89a549af9384bed7a59af69a380ccd46ec54605c
jsimonassi <jsimonassi@id.uff.br>
2024-04-10 02:09:15 -0300

    Remove unused imports

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import {HeaderContainerStyled, InfoGroupStyled} from './styles';
 import {SecondaryText} from '../Text';
-import {Pressable} from 'react-native';
 import {useSessionContext} from '../../contexts/Session';
-import {useNavigation} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
-import {PostAuthRoutesParamList} from '../../types/app/route';
 import {LetterProfileImage} from '../LetterProfileImage';
 import {Helpers} from '../../helpers';
 
